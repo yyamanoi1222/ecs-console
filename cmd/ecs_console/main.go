5c05ec06ed53a6f8f33f54752ec370fccec133a4
Yuu Yamanoi <yuu.yamanoi1222@gmail.com>
2022-11-14 17:58:44 +0900

    wip

--- cmd/ecs_console/main.go
+++ cmd/ecs_console/main.go
@@ -1,17 +1,54 @@
 package main
 
 import (
-  "github.com/yyamanoi1222/ecs_console/internal/runner"
+  "github.com/urfave/cli/v2"
   "os"
   "fmt"
 )
 
 func main() {
-  c := &runner.Config{}
-  parseArg(c)
+  cli.AppHelpTemplate = fmt.Sprintf(`NAME:
+    {{.Name}}
+USAGE:
+    {{.HelpName}} {{if .VisibleFlags}}[options]{{end}}
+OPTIONS:
+    {{range .VisibleFlags}}{{.}}
+    {{end}}
+`)
 
+  app := &cli.App{
+    Name: "ecs_console",
+    Action: func(*cli.Context) error {
+      fmt.Println("ok")
+      return nil
+    },
+    Flags: []cli.Flag{
+      &cli.StringFlag{
+        Name: "cluster",
+        Usage: "ECS Cluster Name",
+      },
+      &cli.StringFlag{
+        Name: "task-def",
+        Usage: "ECS Taskdefinition arn",
+      },
+      &cli.StringFlag{
+        Name: "command",
+        Value: "/bin/bash",
+        Usage: "command passing to ecs-exec",
+      },
+      &cli.StringFlag{
+        Name: "container",
+        Value: "app",
+        Usage: "container name for ecs-exec",
+      },
+    },
+  }
+  app.Run(os.Args)
+
+  /*
   if err := runner.Run(c); err != nil {
     fmt.Printf("error %s \n", err)
     os.Exit(1)
   }
+  */
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,14 @@ module github.com/yyamanoi1222/ecs_console
 
 go 1.18
 
-require github.com/aws/aws-sdk-go v1.44.136
+require (
+	github.com/aws/aws-sdk-go v1.44.136
+	github.com/urfave/cli/v2 v2.23.5
+)
 
-require github.com/jmespath/go-jmespath v0.4.0 // indirect
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
